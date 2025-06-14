a292c936a391413f4af4b964e4c2ba422c653241
Mohit Hasija <mohithasija@Mohits-MacBook-Air.local>
2025-06-14 09:25:09 +0100

    changes made for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -51,13 +52,14 @@ server.use('/wishlist', wishlistRoutes);
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
   // Serve static files from React build
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  server.use(express.static(path.join(__dirname, '../frontend/build')));
 
   // Catch-all handler: send back React's index.html file for any non-API routes
-  app.get('*', (req, res) => {
+  server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });
 }
+
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'running' });
 });
